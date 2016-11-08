e062e61fbd61077e9a5167009d131f96f6ec3396
Mike Hankey <mikehankeyllc@gmail.com>
2016-11-08 14:28:17 +0000

    changed path in status script

--- capture-status.sh
+++ capture-status.sh
@@ -9,9 +9,9 @@ elif [[ $procount -eq 3 ]]; then
      echo "Capture Processes Running..."
 else
      echo "Capture Processes NOT Running. Starting...";
-     /home/pi/capture/capture-stop.sh
+     /home/pi/fireball_camera/capture-stop.sh
      sleep 5
-     /home/pi/capture/capture-start.sh
+     /home/pi/fireball_camera/capture-start.sh
 fi
 
 echo "check defunct"
@@ -19,8 +19,8 @@ defunct=$(ps -aux | grep python3 |grep defunct| wc -l);
 echo $defunct
 if [[ $defunct -eq 1 ]]; then
      echo "Capture Processes Defunct. Restarting..."
-     /home/pi/capture/capture-stop.sh
+     /home/pi/fireball_camera/capture-stop.sh
      sleep 5
-     /home/pi/capture/capture-start.sh
+     /home/pi/fireball_camera/capture-start.sh
 fi
 
