1512322d4cd8628b9ab86d9ffcc479814d264edf
Mike Hankey <mikehankeyllc@gmail.com>
2016-11-22 01:16:53 +0000

    sense up code

--- sense_up.py
+++ sense_up.py
@@ -1,3 +1,4 @@
+import numpy as np
 import requests
 import cv2
 import os
@@ -35,14 +36,13 @@ frame_times = deque(maxlen=200)
 count = 0
 
 print ("Collecting calibration video.")
-while True:
+while count < 91:
    _ , frame = cap.read()
    if _ is True:
       frame_time = time.time()
       frames.appendleft(frame)
       frame_times.appendleft(frame_time)
 
-   count = count + 1
    if count == 90:
       print ("Saving video.")
       dql = len(frame_times) - 1
@@ -59,7 +59,40 @@ while True:
          format_time = format_time + dec_sec
          writer.write(img)
       writer.release()
-      exit()
+   count = count + 1
+
+# sense camera down
+r = requests.get("http://" + config['cam_ip'] + "/webs/btnSettingEx?flag=1000&paramchannel=0&paramcmd=1058&paramctrl=50&paramstep=0&paramreserved=0&")
+exit()
+print("Stack File")
+cap.release()
+time.sleep(2)
+count = 0
+print ("/var/www/html/out/cal/" + outfile)
+cap = cv2.VideoCapture("/var/www/html/out/cal/" + outfile)
+cv2.namedWindow('pepe')
+time.sleep(2)
+count = 0
+image_acc = None
+while count < 90:
+   _ , frame = cap.read()
+   print (count)
+   if frame is None:
+      #print ("eof!", outfile)
+      continue
+
+   #alpha, tstamp_prev = iproc.getAlpha(tstamp_prev)
+   
+   alpha = .23
+   nice_frame = frame
+   #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+   #frame = cv2.GaussianBlur(frame, (21, 21), 0)
+   if image_acc is None:
+      image_acc = np.empty(np.shape(frame))
+   image_diff = cv2.absdiff(image_acc.astype(frame.dtype), frame,)
+   hello = cv2.accumulateWeighted(frame, image_acc, alpha)
+
+   cv2.imshow('pepe', frame)  
+   count = count + 1
 
 os.system("rm /home/pi/fireball_camera/calibrate.txt")
-r = requests.get("http://" + config['cam_ip'] + "/webs/btnSettingEx?flag=1000&paramchannel=0&paramcmd=1058&paramctrl=25&paramstep=0&paramreserved=0&")
