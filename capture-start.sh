323c38820fdaef52c81196eb26296b1df227e137
mikehankey <mikehankeyllc@gmail.com>
2016-12-09 12:32:56 +0000

    hd support

--- capture-start.sh
+++ capture-start.sh
@@ -2,13 +2,13 @@
 
 echo "Trying to start capture...."
 
-procount=$(ps -aux | grep capture.py |grep -v grep| wc -l);
+procount=$(ps -aux | grep capture-light.py |grep -v grep| wc -l);
 echo $procount
-if [[ $procount -eq 4 ]]; then
+if [[ $procount -eq 3 ]]; then
      echo "Capture Processes Already Running. Start aborted."
-elif [[ $procount -eq 3 ]]; then
+elif [[ $procount -eq 2 ]]; then
      echo "Capture Processes Already Running. Start aborrted."
 else
      echo "Capture Processes NOT Running. Starting...";
-     sudo -H -u pi ./capture.py &
+     sudo -H -u pi ./capture-light.py &
 fi
--- capture-status.sh
+++ capture-status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-procount=$(ps -aux | grep capture.py |grep -v grep| wc -l);
+procount=$(ps -aux | grep capture-light.py |grep -v grep| wc -l);
 echo $procount
-if [[ $procount -eq 4 ]]; then
+if [[ $procount -eq 3 ]]; then
      echo "Capture Processes Running"
-elif [[ $procount -eq 3 ]]; then
+elif [[ $procount -eq 2 ]]; then
      echo "Capture Processes Running..."
 else
      echo "Capture Processes NOT Running. Starting...";
@@ -15,7 +15,7 @@ else
 fi
 
 echo "check defunct"
-defunct=$(ps -aux | grep capture.py |grep defunct| wc -l);
+defunct=$(ps -aux | grep capture-light.py |grep defunct| wc -l);
 echo $defunct
 if [[ $defunct -eq 1 ]]; then
      echo "Capture Processes Defunct. Restarting..."
