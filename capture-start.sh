4b3302aeb313cac21a31bdfbc54a87406ef07d17
mikehankey <mikehankeyllc@gmail.com>
2016-12-12 18:20:06 +0000

    new start/stop csripts

--- capture-start.sh
+++ capture-start.sh
@@ -2,7 +2,7 @@
 
 echo "Trying to start capture...."
 
-procount=$(ps -aux | grep capture-light.py |grep -v grep| wc -l);
+procount=$(ps -aux | grep capture-hd.py |grep -v grep| wc -l);
 echo $procount
 if [[ $procount -eq 3 ]]; then
      echo "Capture Processes Already Running. Start aborted."
@@ -10,5 +10,5 @@ elif [[ $procount -eq 2 ]]; then
      echo "Capture Processes Already Running. Start aborrted."
 else
      echo "Capture Processes NOT Running. Starting...";
-     sudo -H -u pi ./capture-light.py &
+     sudo -H -u pi ./capture-hd.py &
 fi
--- capture-status.sh
+++ capture-status.sh
@@ -3,9 +3,9 @@
 
 procount=$(ps -aux | grep capture-light.py |grep -v grep| wc -l);
 echo $procount
-if [[ $procount -eq 3 ]]; then
+if [[ $procount -eq 4 ]]; then
      echo "Capture Processes Running"
-elif [[ $procount -eq 2 ]]; then
+elif [[ $procount -eq 3 ]]; then
      echo "Capture Processes Running..."
 else
      echo "Capture Processes NOT Running. Starting...";
