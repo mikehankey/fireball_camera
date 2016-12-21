44c769a812a4f7265a0d75c61b15515ef5e0f6b6
mikehankey <mikehankeyllc@gmail.com>
2016-12-21 01:53:39 +0000

    isense up

--- sense_up.py
+++ sense_up.py
@@ -168,13 +168,13 @@ def stack_calibration_video(outfile):
 
 
    jpg_file = outfile.replace(".avi", ".jpg")
-   cv2.imwrite(jpg_file, dst)
+   cv2.imwrite("/var/www/html/out/cal/" + jpg_file, dst)
    jpg_file_x = outfile.replace(".avi", "-x.jpg")
-   cv2.imwrite(jpg_file_x, dst_x)
+   cv2.imwrite("/var/www/html/out/cal/" + jpg_file_x, dst_x)
    jpg_file = jpg_file.replace(".jpg", "-single.jpg")
    print (jpg_file)
    sframe = cv2.convertScaleAbs(sframe)
-   cv2.imwrite(jpg_file, sframe)
+   cv2.imwrite("/var/www/html/out/cal/" + jpg_file, sframe)
 
    #img_filt = cv2.medianBlur(cv2.imread('jpg_file',0), 5)
    #img_th = cv2.adaptiveThreshold(img_filt,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
