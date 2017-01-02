1a6b177b5bbd0ad6bd0b4f58d0222e7f3c6a36ef
mikehankey <mikehankeyllc@gmail.com>
2017-01-02 15:15:28 +0000

    api script for latest upload

---
+++ upload_latest.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import mimetypes
+import sys
+import datetime
+import time
+
+from amscommon import read_config
+
+config = read_config()
+
+# UPLOAD LATEST CAM FRAME (every hour)
+
+api_key = config['ams_api_key']
+cam_id  = config['cam_id']
+url = "http://www.amsmeteors.org/members/api/cam_api/upload_latest"
+file = "/var/www/html/out/latest.jpg"
+stat = os.stat(file)
+print (stat)
+#datetime = stat.st_birthtime
+dt = datetime.datetime.fromtimestamp(stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+ 
+# usage: python upload.py type misc_info datetime filename 
+# ex: python uploadLatest.py 2016-09-09%2020:03:02 some_info test.jpg
+
+  
+#datetime = sys.argv[1]
+#misc_info = sys.argv[2]
+
+# The File to send
+_file = {'file_data': open(file, 'rb')}
+
+# The Data to send with the file
+_data= {'api_key': api_key, 'cam_id': cam_id, 'datetime': dt}
+ 
+session = requests.Session()
+del session.headers['User-Agent']
+del session.headers['Accept-Encoding'] 
+
+with requests.Session() as session:
+    response = session.post(url, data= _data, files=_file)
+
+
+print (response.text)
+response.raw.close() 
