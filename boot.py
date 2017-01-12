143494c365e995dcd675bcc42b0ab7bbd05a801b
mikehankey <mikehankeyllc@gmail.com>
2017-01-12 19:04:18 +0000

    boot script

--- boot.py
+++ boot.py
@@ -2,7 +2,7 @@
 
 import os
 from amscommon import read_config
-
+import netifaces
 # system boot script runs each time system is booted. 
 # Handles first time registration and setup
 # once setup simply does sanity check and logs to ams 
@@ -20,4 +20,15 @@ fp.write("booted")
 fp.close()
 
 os.system("./mkdevice.py")
-os.system("./logger.py reboot")
+
+try:
+    eth0_ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+except:
+    eth0_ip = "0.0.0.0"
+try:
+    wlan0_ip= netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+except:
+    wlan0_ip = "0.0.0.0"
+
+msg = "reboot " + eth0_ip + "/" + wlan0_ip
+os.system("./logger.py '" + msg + "'")
