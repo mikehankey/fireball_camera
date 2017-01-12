2cba3fd29db20113ee59f76250408dccd035c579
mikehankey <mikehankeyllc@gmail.com>
2017-01-12 16:05:39 +0000

    boot scripts

--- boot.py
+++ boot.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3 
 
+from amscommon import read_config
+
 # system boot script runs each time system is booted. 
 # Handles first time registration and setup
 # once setup simply does sanity check and logs to ams 
+
+config = read_config()
+try:
+   if (config['device_lat'] != ''):
+      print ("setup.")
+except:
+   print ("device not setup yet.")
+
 
 fp = open("/home/pi/fireball_camera/booted.txt", "w")
 fp.write("booted")
--- logger.py
+++ logger.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+
 import requests
 import sys
-api_key = "APIKEYHERE!"
+from amscommon import read_config
 
 # usage: python logger.py type msg
 # type = type of log message (reboot, system, capture, calibration) 
 # msg = log message 
 
+config = read_config()
 
-type = sys.argv[1]
-msg = sys.argv[2]
+msg = sys.argv[1]
 
-r = requests.post('http://localhost/test.php', data={'api_key': api_key, 'type': type, 'msg': msg})
-print r.text
+r = requests.post('http://www.amsmeteors.org/members/api/cam_api/addLog', data={'device_id': config['device_id'], 'api_key': config['api_key'], 'msg': msg})
+print (r.text)
