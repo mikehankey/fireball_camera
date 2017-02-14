c8f0e285ad58c14ee77a43cce860b3aa5a8b4c1e
Mike Hankey <mike.hankey@gmail.com>
2017-02-14 17:30:43 +0000

    setup fixes

--- boot.py
+++ boot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3 
 
 import os
+import time
 from amscommon import read_config
 import netifaces
+time.sleep(30)
 # system boot script runs each time system is booted. 
 # Handles first time registration and setup
 # once setup simply does sanity check and logs to ams 
@@ -19,7 +21,7 @@ fp = open("/home/pi/fireball_camera/booted.txt", "w")
 fp.write("booted")
 fp.close()
 
-os.system("cd /home/pi/fireball_camera; ./mkdevice.py")
+os.system("cd /home/pi/fireball_camera; ./mkdevice.py >> booted.txt")
 
 try:
     eth0_ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
@@ -31,4 +33,4 @@ except:
     wlan0_ip = "0.0.0.0"
 
 msg = "reboot " + eth0_ip + "/" + wlan0_ip
-os.system("cd /home/pi/fireball_camera; ./logger.py '" + msg + "'")
+os.system("cd /home/pi/fireball_camera; ./logger.py '" + msg + "' >> booted.txt")
