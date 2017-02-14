2d28acf8bcd6945be04a5d9f11edf24fbf1f0cc3
Mike Hankey <mike.hankey@gmail.com>
2017-02-14 18:53:11 +0000

    start up fixes

--- mkdevice.py
+++ mkdevice.py
@@ -38,11 +38,39 @@ print ("WLAN MAC: ", wlan0_mac)
 print ("ETH0 IP: ", eth0_ip)
 print ("WLAN IP: ", wlan0_ip)
 
-r = requests.get('http://www.amsmeteors.org/members/api/cam_api/mkdevice?format=json&LAN_MAC=' + eth0_mac + '&WLAN_MAC=' + wlan0_mac + '&lan_ip=' + eth0_ip + 'wlan_ip=' + wlan0_ip)
-#print (r.text)
-fp = open("register.txt", "w")
-fp.write(r.text)
-fp.close()
+try:
+   r = requests.get('http://www.amsmeteors.org/members/api/cam_api/mkdevice?format=json&LAN_MAC=' + eth0_mac + '&WLAN_MAC=' + wlan0_mac + '&lan_ip=' + eth0_ip + 'wlan_ip=' + wlan0_ip)
+   fp = open("register.txt", "w")
+   fp.write(r.text)
+   fp.close()
+except:
+   print ("mkdevice failed.")
+
+data = json.loads(r.text)
+try:
+   if data['errors']['Invalid_data'] == 'LAN_MAC WLAN_MAC combination must be unique.':
+      print ("Device already created.")
+   else:
+      print ("Device created.")
+except:
+   print ("Device Created.")
+  
+#LOG IP OF DEVICE. 
+msg = "lan_ip=" + eth0_ip + ":wlan_ip=" + wlan0_ip
+r = requests.post('http://www.amsmeteors.org/members/api/cam_api/addLog', data={'LAN_MAC': eth0_mac, 'WLAN_MAC': wlan0_mac, 'msg': msg})
+
+res = r.text
+
+x, id = res.split("device_id: ")
+
+hostname = "ams" + id.rstrip("\n")
+print ("ID: ", hostname)
+out = open("/home/pi/fireball_camera/host", "w") 
+out.write(hostname)
+out.close()
+os.system("sudo cp /home/pi/fireball_camera/host /etc/hostname")
+
+exit()
 
 # GET THE DEVICE INFO
 
@@ -53,27 +81,25 @@ fp.write(r.text)
 fp.close()
 
 data = json.loads(r.text)
-for key in data['result'][1]:
-   print (key)
+try: 
+   for key in data['result'][1]:
+      print (key)
 
-dev_data = data['result'][0]['cam'][0]
-operator_data = data['result'][1]['operator'][0]
-for key in operator_data:
-   if type(operator_data[key]) is str:
-      print (key, operator_data[key])
-      config[key]=operator_data[key]
+   dev_data = data['result'][0]['cam'][0]
+   operator_data = data['result'][1]['operator'][0]
+   for key in operator_data:
+      if type(operator_data[key]) is str:
+         print (key, operator_data[key])
+         config[key]=operator_data[key]
 
-for key in dev_data:
-   if type(dev_data[key]) is str:
-      print (key, dev_data[key])
-      config[key]=dev_data[key]
-print ("CONFIG")
-print (config)
-print ("CONFIG")
-write_config(config)
-put_device_info(config)
-print (operator_data)
+   for key in dev_data:
+      if type(dev_data[key]) is str:
+         print (key, dev_data[key])
+         config[key]=dev_data[key]
+   write_config(config)
+   put_device_info(config)
+except: 
+   print ("Device is not claimed yet.")
 
 
-exit()
 
