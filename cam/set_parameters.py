d1b78c493ca23737efaa33507c5a7eeac1142a50
vperlerin <vperlerin@gmail.com>
2017-05-23 12:32:25 +0000

    Update Cam Parameters

--- cam/set_parameters.py
+++ cam/set_parameters.py
@@ -16,10 +16,11 @@ config = read_config()
 possible_values = ['Brightness','Contrast','Saturation','Chroma'];
    
 #ex: .../cgi-bin/videoparameter_cgi?action=set&channel=0&user=admin&pwd=admin&Brightness=20&Contrast=100&Chroma=200&Saturation=123
+ 
    
 # Get Parameters passed in arg
 if(len(sys.argv)!=1):
-
+ 
     new_values  = json.loads(sys.argv[1]) # Config info
     
     parameters = ''
@@ -27,16 +28,16 @@ if(len(sys.argv)!=1):
     for key in new_values:
         if key in possible_values:
             parameters += '&' + key + '=' +  str(new_values[key])
-        
+ 
     
     if(parameters != ''):
         # Set the new Cam Video Parameters
         fname = 'http://'+config['cam_ip']+'/cgi-bin/videoparameter_cgi?action=set&channel=0&user=admin&pwd='+config['cam_pwd'] + parameters
         
         # Call to CGI
-        urllib.urlopen(fname)
+        a = urllib.urlopen(fname)
     
-        print 'Parameters Updated'
+        print a.getcode()
  
     else:
         print 'No Parameters to Update'
