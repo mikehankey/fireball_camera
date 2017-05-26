7e0d3ea30ab0ca783dc5b6b5eef36e265cabf33a
vperlerin <vperlerin@gmail.com>
2017-05-26 13:20:15 +0000

    Some cleaning

--- config/read_config.py
+++ config/read_config.py
@@ -12,4 +12,23 @@ from crypt import Crypt
 
 config = read_config() 
  
+try:
+    config['az_left']  = int(config['cam_heading']) - (int(config['cam_fov_x'])/2)
+    config['az_right'] = int(config['cam_heading']) + (int(config['cam_fov_x'])/2)
+    if (config['az_right'] > 360):
+        config['az_right'] = config['az_right'] - 360
+    if (config['az_left'] > 360):
+        config['az_left'] = config['az_left'] - 360
+    if (config['az_left'] < 0):
+        config['az_left'] = config['az_left'] + 360
+        config['el_bottom'] = int(config['cam_alt']) - (int(config['cam_fov_y'])/2)
+        config['el_top'] = int(config['cam_alt']) + (int(config['cam_fov_y'])/2)
+    # Calibration Done    
+    config['calibration'] = '1'
+except:
+    config['az_left'] = 0
+    config['az_right'] = 0
+    # Calibration TO BE DONE
+    config['calibration'] = '0'
+
 print json.dumps(config, ensure_ascii=False, encoding="utf-8")
