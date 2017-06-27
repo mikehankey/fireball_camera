16e4a712d08f440418884309392be5d6adef0f34
vperlerin <vperlerin@gmail.com>
2017-06-27 11:31:48 +0000

    Log on Config

--- config/config_func.py
+++ config/config_func.py
@@ -1,12 +1,18 @@
 import os
 import sys
 import json
+import logging
+
+
+logging.basicConfig(filename='/home/pi/fireball_camera/log_files/app.log',format='%(asctime)s -- %(levelname)s: --  %(message)s', datefmt='%m/%d/%Y %H:%M:%S',level=logging.DEBUG)
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../pwd')))
 from crypt import Crypt
 
 # Read Config File
 def read_config():
+
+    logging.debug('Reading Config.txt')
     config = {}
     
     try: 
@@ -23,19 +29,24 @@ def read_config():
         config['hd'] = 0
         
         if 'cam_pwd' in config:
+            
             try:
                 #We decrypt the cam password if it is crypted
                 c = Crypt() 
                 config['cam_pwd']  = c.decrypt(config['cam_pwd'])
+                logging.debug('cam_pwd successfully decrypted')
             except:
-                 config['error'] = "Impossible to decrypt the password - password must only contains characters and digits."
-        
+                config['error'] = "Impossible to decrypt the password - password must only contains characters and digits"
+                logging.error('Impossible to decrypt ' +  str(config['cam_pwd']))
+                
         file.close()
+        logging.debug('Result ' + str(config) )
+        
         return(config)
     except:
-        config['error'] = 'The config file cannot be read, please check your config.txt file.';
-         
-    
+        config['error'] = 'The config file cannot be read, please check your config.txt file';
+        logging.error('Error in config.txt')
+        logging.debug( str(config))
     return config;
 
     
@@ -48,6 +59,7 @@ def get_config():
 # Read the config file and parse FOV & EL
 # And returns array
 def read_config_raw():
+  
     config = read_config() 
     
     try:
@@ -62,10 +74,12 @@ def read_config_raw():
             config['el_bottom'] = int(config['cam_alt']) - (int(config['cam_fov_y'])/2)
             config['el_top']    = int(config['cam_alt']) + (int(config['cam_fov_y'])/2)
         config['calibration']   = '1' #Calibration Done  
+        logging.debug('Calibration info computed from config.txt')
     except:
         config['az_left']   = 0
         config['az_right']  = 0
         config['calibration'] = '0' #Calibration TO BE DONE
+        logging.warning('Calibration info missing in config.txt')
     return config;
 
 
@@ -73,34 +87,43 @@ def read_config_raw():
 # WARNING THIS FUNCTION DOESNT UPDATE THE cam_pwd ON THE CAMERA SIDE 
 def add_to_config(param,new_value):
     #Read Current Config
-    config = read_config_raw();
     
-    updated = 0;
+    if(param!='cam_pwd'):
     
-    #Loop throught config
-    for key in config:
+        config = read_config_raw();
         
-        if(key == param):
-            config[key] = new_value
-            updated = 1;
-    
-    # In case the param doesnt exist yet in the config file
-    if(updated==0):
-        config[param] = new_value
+        logging.debug('Try to update the config file with ' +  str(param) + ' = ' + str(new_value))
         
-    # We rewrite the config file
-    # WARNING: we need to recrypt cam_pwd here as read_config_raw decrypt it   
-    
-    file = open("/home/pi/fireball_camera/config.txt", "w")
-    
-    for key in config:
-        value = config[key]
+        updated = 0;
         
-        if(key=='cam_pwd'):
-            c     = Crypt()
-            value = c.encrypt(value)
+        #Loop throught config
+        for key in config:
+            
+            if(key == param):
+                config[key] = new_value
+                updated = 1;
         
-        line = str(key) + "=" + str(value) + "\n"
-        file.write(line) 
-               
-    file.close()
+        # In case the param doesnt exist yet in the config file
+        if(updated==0):
+            config[param] = new_value
+            logging.debug('The parameter ' + str(param) + ' didnt exist in config.txt')
+            
+        # We rewrite the config file
+        # WARNING: we need to recrypt cam_pwd here as read_config_raw decrypt it   
+        
+        file = open("/home/pi/fireball_camera/config.txt", "w")
+        
+        for key in config:
+            value = config[key]
+            
+            if(key=='cam_pwd'):
+                c     = Crypt()
+                value = c.encrypt(value)
+            
+            line = str(key) + "=" + str(value) + "\n"
+            file.write(line) 
+                   
+        file.close()
+        
+    else:
+            logging.error('We tried to update cam_pwd with add_to_config: IMPOSSIBLE')
--- config/update_config.py
+++ config/update_config.py
@@ -10,8 +10,13 @@ import pprint
 
 from config_func import read_config_raw
 
+import logging
+logging.basicConfig(filename='/home/pi/fireball_camera/log_files/app.log',format='%(asctime)s -- %(levelname)s: --  %(message)s', datefmt='%m/%d/%Y %H:%M:%S',level=logging.DEBUG)
+
 sys.path.insert(1, os.path.join(sys.path[0], '../pwd'))
 from crypt import Crypt
+
+logging.debug('Updating config.txt')
   
 config  = json.loads(sys.argv[1]) # Config info
  
@@ -25,45 +30,60 @@ for key in config:
  
     # We need to update the cam password 
     if key == 'new_cam_pwd':
+    
+        logging.debug('Updating cam_pwd')
         
         # It is the first time
         if 'cam_pwd' in config:
+        
+            logging.debug('cam_pwd already exist in config.txt - we update it')
             
             #Update the Cam Password via the cgi
             fname = 'http://'+config['cam_ip']+'/cgi-bin/pwdgrp_cgi?action=update&user=admin&pwd='+config['cam_pwd']+'&username=admin&password='+value
             #print fname
             
+            logging.debug('Updating the cam_pwd on the camera side:' + fname)
+             
             # Remove the old cam_pwd so we dont have 2 in the config file
             del tmp_config['cam_pwd']
           
         # It is not the first time
         else:
+        
+            logging.debug('cam_pwd does NOT exist in config.txt - we add it')
              
             #Update the Cam Password via the cgi
             fname = 'http://'+config['cam_ip']+'/cgi-bin/pwdgrp_cgi?action=update&user=admin&pwd=admin&username=admin&password='+value
-            #print fname
+            logging.debug('Updating the cam_pwd on the camera side:' + fname)
+
          
         # Call to CGI
         urllib.urlopen(fname)
          
-        # Encrypt the Cam Password to store it in the config file
-        c     = Crypt()
-        value = c.encrypt(value)
-   
-      
-        # Write new pwd in config.txt
-        line = "cam_pwd=" + str(value) + "\n"
-        file.write(line)
+        # Encrypt the Cam Password to store it in the config file 
+        try:
+            c     = Crypt()
+            value = c.encrypt(value)
+            logging.debug('cam_pwd successfully encrypted') 
+            
+            # Write new pwd in config.txt
+            line = "cam_pwd=" + str(value) + "\n"
+            file.write(line)   
+        except:
+            logging.error('IMPOSSIBLE to encrypted the following cam_pwd ' + str(value))
         
         del tmp_config['new_cam_pwd']
 
- 
+
+logging.debug('Updating config.txt') 
 for key in tmp_config:
     value = tmp_config[key]
     line = str(key) + "=" + str(value) + "\n"
+    logging.debug(str(key) + "=" + str(value)) 
     file.write(line)
         
 file.close()
+logging.debug('config.txt updated') 
 
 #Read again before sending  
 configN = read_config_raw();
