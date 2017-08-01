d06a78e06d28fb84ba98b928dfd7b7df2b62fed6
vperlerin <vperlerin@gmail.com>
2017-08-01 13:29:44 +0000

    Debug & Mail Functions

--- mail/send_email_functions.py
+++ mail/send_email_functions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import sys
 import smtplib
  
@@ -6,22 +5,21 @@ import smtplib
 def send_email(to,subject,message,sent_from='Your AMSCam <noreply@castlecomm.com>'): 
 
     email_text = """From: %s  
-    To: %s
-    MIME-Version: 1.0
-    Content-type: text/html  
-    Subject: %s
-    
-    %s
-    """ % (sent_from, ", ".join(to), subject, message)
+To: %s
+MIME-Version: 1.0
+Content-type: text/html  
+Subject: %s
 
-    gmail_user = 'noreply@castlecomm.com'  
-    gmail_password = '1ancel011ancel01'
+%s
+""" % (sent_from, ", ".join(to), subject, message)
+    
+    print(email_text)
  
     try: 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
-        server.login(gmail_user, gmail_password)
+        server.login('noreply@castlecomm.com', '1ancel011ancel01')
         server.sendmail(sent_from, to, email_text)
         server.close()
         print ("Message sent")
