41e90dfb1027cdc2741e4cd875ba097807ba2dc5
ams <mike.hankey@gmail.com>
2017-12-08 16:22:24 +0000

    iptables

--- iptables.sh
+++ iptables.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 iptables --table nat --append POSTROUTING --out-interface enp1s0 -j MASQUERADE
 iptables --append FORWARD --in-interface enp3s0 -j ACCEPT
+
+iptables --table nat --append POSTROUTING --out-interface enp3s0 -j MASQUERADE
+iptables --append FORWARD --in-interface enp1s0 -j ACCEPT
