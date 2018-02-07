31b062c1377390d4ae8dbdb0a073d900e136ce79
Mike Hankey <mike.hankey@gmail.com>
2018-02-07 11:27:44 +0000

    update

--- camera_time.py
+++ camera_time.py
@@ -3,7 +3,7 @@
 # important backdoor URLS
 # OSD - /videoosd.asp?
 # Settings - /videolens.asp
-
+import datetime
 import sys
 import requests
 from amscommon import read_config
@@ -28,8 +28,20 @@ cam_ip = config['cam_ip']
 
 print ("Setting up defaults for camera on IP address:", cam_ip)
 
-print ("Set timezone and NTP server.")
-url = "http://" + str(cam_ip) + "/cgi-bin/date_cgi?action=set&user=admin&pwd="+ config['cam_pwd'] +"&timezone=14&ntpHost=clock.isc.org"
+cur_datetime = datetime.datetime.now()
+req_str = "year=" + str(cur_datetime.strftime("%Y")) + "&" + "month=" + str(cur_datetime.strftime("%m")) + "&" + "day=" + str(cur_datetime.strftime("%d")) + "&" + "hour=" + str(cur_datetime.strftime("%H")) + "&" + "minute=" + str(cur_datetime.strftime("%M")) + "&" + "second=" + str(cur_datetime.strftime("%S"))
+
+print (req_str)
+
+print ("Get time from camera.")
+url = "http://" + str(cam_ip) + "/cgi-bin/date_cgi?action=get&user=admin&pwd="+ config['cam_pwd'] 
+print (url)
+r = requests.get(url)
+print (r.text)
+
+
+print ("Set time, timezone and NTP server.")
+url = "http://" + str(cam_ip) + "/cgi-bin/date_cgi?action=set&user=admin&pwd="+ config['cam_pwd'] +"&timezone=14&ntpHost=clock.isc.org&" + req_str
 print (url)
 r = requests.get(url)
 print (r.text)
