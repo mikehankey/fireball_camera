f49562af650c31a4e07f87ada6edc46256658885
Mike Hankey <mike.hankey@gmail.com>
2018-02-27 15:54:27 +0000

    update

---
+++ update-dirs.sh
@@ -0,0 +1,13 @@
+mkdir /var/www/html/out/calvid
+mkdir /var/www/html/out/cal
+mkdir /var/www/html/out/dist
+mkdir /var/www/html/out/temp_upload
+mkdir /var/www/html/out/time_lapse
+mkdir /var/www/html/out/time_lapse/1
+mkdir /var/www/html/out/time_lapse/2
+mkdir /var/www/html/out/time_lapse/3
+mkdir /var/www/html/out/time_lapse/4
+mkdir /var/www/html/out/time_lapse/5
+mkdir /var/www/html/out/time_lapse/6
+mkdir /var/www/html/out/time_lapse/trash
+mkdir /var/www/html/out/time_lapse/videos
