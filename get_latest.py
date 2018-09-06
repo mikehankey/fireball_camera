11637b6f55d0843e04d444c2eff7dd97a65c871c
ams <mike.hankey@gmail.com>
2018-09-06 16:30:01 +0000

    latest view

---
+++ get_latest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from collections import defaultdict
+from PIL import Image, ImageChops
+import numpy as np
+from pathlib import Path
+import requests
+import cv2
+import os
+import time
+import datetime
+import sys
+from collections import deque
+import iproc
+from amscommon import read_sun, read_config
+
+
+def get_latest_pic(cam_num):
+   config_file = "conf/config-" + str(cam_num) + ".txt"
+   outfile = "/var/www/html/out/latest-" + str(cam_num) + ".jpg"
+
+   config = read_config(config_file)
+   cap = cv2.VideoCapture("rtsp://" + config['cam_ip'] + "/av0_0")
+
+
+   cv2.setUseOptimized(True)
+
+   _ , frame = cap.read()
+   #frame = cv2.resize(frame, (0,0), fx=1, fy=.75)
+
+   cv2.imwrite(outfile, frame)  
+
+
+cam_num = sys.argv[1]
+
+get_latest_pic(cam_num)
