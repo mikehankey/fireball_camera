3bcf75f42581d5575a6694d2accbc324930961c8
ams <mike.hankey@gmail.com>
2018-10-30 16:43:25 +0000

    mkd

---
+++ mkdirs.sh
@@ -0,0 +1,11 @@
+mkdir /mnt/ams2/SD
+mkdir /mnt/ams2/HD
+mkdir /mnt/ams2/SD/proc
+mkdir /mnt/ams2/SD/proc/daytime
+mkdir /mnt/ams2/meteors
+mkdir /mnt/ams2/saved
+mkdir /mnt/ams2/saved/planes
+mkdir /mnt/ams2/saved/sats
+mkdir /mnt/ams2/saved/interesting
+mkdir /mnt/ams2/cal
+mkdir /mnt/ams2/cal/solved
