5e20df72466a52bf2aa7e2fd574cd8c4bb438b34
ams <mike.hankey@gmail.com>
2018-11-06 16:09:27 +0000

    setup

--- setup-drive.sh
+++ setup-drive.sh
@@ -1,9 +1,17 @@
-#sudo mkfs -t ext4 /dev/sda1
-chown -R ams:ams /mnt/ams2
+sudo umount /dev/sda1
+sudo mkfs -t ext4 /dev/sda1
+sudo mount /dev/sda1
+sudo chown -R ams:ams /mnt/ams2
 mkdir /mnt/ams2/SD
+mkdir /mnt/ams2/SD/proc
 mkdir /mnt/ams2/SD/proc/daytime
 mkdir /mnt/ams2/HD/
 mkdir /mnt/ams2/meteors
-mkdir /mnt/ams2/sats
-mkdir /mnt/ams2/interesting
-mkdir /mnt/ams2/planes
+mkdir /mnt/ams2/cal/
+mkdir /mnt/ams2/cal/solved
+mkdir /mnt/ams2/cal/bad
+mkdir /mnt/ams2/cal/failed
+mkdir /mnt/ams2/saved/
+mkdir /mnt/ams2/saved/sats
+mkdir /mnt/ams2/saved/interesting
+mkdir /mnt/ams2/saved/planes
